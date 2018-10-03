5d0d774806be373174493b41efec559817ad43a2
AdrianGarciaB <33894550+AdrianGarciaB@users.noreply.github.com>
2018-10-03 11:07:21 +0200

    Delete forlan.sh

--- forlan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-imagen="=/Imagen.jpeg\="
-
-# Recorte de etiquetas
-echo $imagen | cut -d "=" -f2 | cut -d "/" -f2 | cut -d '\' -f1
-echo $imagen | cut -d "/" -f2 | rev | cut -c 2- | rev
-
